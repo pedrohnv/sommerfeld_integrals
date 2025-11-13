95367e2276d20d954005cbd149ad43a0d33cf207
pedrohnv <phnvieira@pm.me>
2025-11-13 15:12:40 -0300

    separate test and module

--- makefile
+++ makefile
@@ -2,19 +2,24 @@ FC=gfortran
 FFLAGS=-fPIC
 slateclib = ./slatec/lib/libslatec.a
 
-all: clean quadde_test mosig_michalski
+all: clean quadde_test mosig_michalski_PE_test
 
 clean:
-	rm -f *.o *.mod *.out
+	rm -f *.o *.mod
 
 quadde_module:
 	$(FC) $(FFLAGS) -c quadde_module.f90
 
 quadde_test: quadde_module
-	$(FC) $(FFLAGS) -o a.out quadde_test.f90 quadde_module.o && ./a.out
+	$(FC) $(FFLAGS) -o test quadde_test.f90 quadde_module.o && ./test
+	rm test
 
-mosig_michalski: quadde_module $(slateclib)
-	$(FC) $(FFLAGS) -o a.out mosig_michalski_PE.f90 quadde_module.o $(slateclib) && ./a.out
+mosig_michalski_PE_module: quadde_module
+	$(FC) $(FFLAGS) -c mosig_michalski_PE_module.f90
+
+mosig_michalski_PE_test: quadde_module mosig_michalski_PE_module $(slateclib)
+	$(FC) $(FFLAGS) -o test mosig_michalski_PE_test.f90 quadde_module.o mosig_michalski_PE_module.o $(slateclib) && ./test
+	rm test
 
 $(slateclib): slatec
 	cd slatec && make FC=$(FC) && cd ..
--- mosig_michalski_PE_module.f90
+++ mosig_michalski_PE_module.f90
@@ -15,7 +15,7 @@
 !> # Referências
 !> - MICHALSKI, Krzysztof A.; MOSIG, Juan R. Efficient computation of Sommerfeld integral tails – methods and algorithms. Journal of Electromagnetic Waves and Applications, v. 30, n. 3, p. 281-317, 2016.
 !> - MICHALSKI, K. A. On the efficient evaluation of integral arising in the sommerfeld halfspace problem. In: IEE Proceedings H (Microwaves, Antennas and Propagation). IET Digital Library, 1985. p. 312-318.
-module Mosig_Michalski_PE
+module Mosig_Michalski_PE_module
     use iso_fortran_env, only: wp => real64  ! working precision
     use quadde_module  ! quadde, integrable_function
     implicit none
@@ -190,105 +190,4 @@ contains
         deallocate(partial_sums)
     end subroutine
 
-end module Mosig_Michalski_PE
-
-
-program test_mosig_michalski
-    use, intrinsic :: ieee_arithmetic
-    use Mosig_Michalski_PE
-    implicit none
-
-    real(kind=wp), parameter :: PI = 3.141592653589793238462643383279502884197169399375105820974944592307816406286198_wp
-    integer, parameter :: N = 20
-    real(kind=wp) :: v, p, a, q, tol, alpha, u, z
-    complex(kind=wp) :: series_term, partial_sums(N), extrapolated_sum, val, error_estimate
-    integer :: i, kmax
-    logical :: is_nan
-
-    print *, "Testing mosig_michalski_PE..."
-
-    ! Series acceleration
-    u = 1.0
-    extrapolated_sum = (0.0, 0.0)
-
-    ! Example 1
-    do i = 0, N-1
-        series_term = (-1.0_wp)**real(i) / sqrt(real(i+1))
-        if (i == 0) then
-            partial_sums(i + 1) = series_term
-        else
-            partial_sums(i + 1) = partial_sums(i) + series_term
-        end if
-    end do
-    extrapolated_sum = mosig_michalski(partial_sums, u)
-    is_nan = ieee_is_nan(real(extrapolated_sum)) .or. ieee_is_nan(aimag(extrapolated_sum))
-    if (abs(real(extrapolated_sum) - 0.604898643421630_wp) > 1e-6 .or. is_nan) stop "Test 1 failed"
-
-    ! Example 2
-    do i = 0, N-1
-        series_term = (4.0_wp / 5.0_wp)**real(i+1) / real(i+1)
-        if (i == 0) then
-            partial_sums(i + 1) = series_term
-        else
-            partial_sums(i + 1) = partial_sums(i) + series_term
-        end if
-    end do
-    extrapolated_sum = mosig_michalski(partial_sums, u)
-    is_nan = ieee_is_nan(real(extrapolated_sum)) .or. ieee_is_nan(aimag(extrapolated_sum))
-    if (abs(real(extrapolated_sum) - log(5.0_wp)) > 1e-6 .or. is_nan) stop "Test 2 failed"
-
-    ! Example 3
-    do i = 0, N-1
-        series_term = 1.0_wp / real(i+1)**2.0_wp
-        if (i == 0) then
-            partial_sums(i + 1) = series_term
-        else
-            partial_sums(i + 1) = partial_sums(i) + series_term
-        end if
-    end do
-    extrapolated_sum = mosig_michalski(partial_sums, u)
-    is_nan = ieee_is_nan(real(extrapolated_sum)) .or. ieee_is_nan(aimag(extrapolated_sum))
-    if (abs(real(extrapolated_sum) - (PI**2.0_wp / 6.0_wp)) > 1e-2 .or. is_nan) stop "Test 3 failed"
-
-    ! Partition-Extrapolation
-    v = 2.0
-    p = 1.0
-    a = 5.13562
-    q = PI / p
-    z = 0.0
-    tol = 1e-9
-    alpha = 1.0 / 2.0 - v
-    kmax = 10
-    u = 2.0
-    ! It is safe to call f(x) onward
-    call part_extrap(f, a, q, z, alpha, tol, kmax, u, val, error_estimate)
-    print *, "I =", val
-    print *, "E =", error_estimate
-    is_nan = ieee_is_nan(real(val)) .or. ieee_is_nan(aimag(val))
-    if (abs(-10.07948621951323 - val) > 1e-6 .or. is_nan) stop "Partition-Extrapolation failed"
-
-    print *, "All tests passed!"
-
-contains
-
-    function f(x) result(y)
-        real(wp), intent(in) :: x
-        real(wp) :: ZR, ZI, CYR, CYI, FNU
-        complex(wp) :: y, besselj
-        integer :: IERR, NT, NZ, KODE
-        NT = 1
-        NZ = 1
-        KODE = 1  ! no scaling
-        FNU = v
-        ZR = x
-        ZI = 0.0
-        IERR = 0
-        call ZBESJ(ZR, ZI, FNU, KODE, NT, CYR, CYI, NZ, IERR)
-        if (IERR /= 0) then
-            print *, "IERR =", IERR
-        end if
-        besselj = cmplx(CYR, CYI, kind=wp)
-        y = exp(-x * z) * besselj * x**v
-    end function
-
-end program test_mosig_michalski
+end module Mosig_Michalski_PE_module
